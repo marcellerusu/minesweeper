dbfc683c90250c72e22298c2e63b317b51990165
MarcelRusu <marcelrusu@protonmail.com>
2025-02-21 23:38:41 -0500

    clean up Game#click

--- src/main.ts
+++ src/main.ts
@@ -101,44 +101,22 @@ class Game {
       this.open_unless_flagged(cell.x, cell.y);
   }
 
-  #try_click_at(
-    x: number,
-    y: number
-  ):
-    | "open"
-    | "wide_open"
-    | "clicked_on_mine"
-    | "already_open"
-    | "out_of_range" {
-    if (x < 0 || x >= this.WIDTH || y < 0 || y >= this.HEIGHT)
-      return "out_of_range";
-    if (this.#board[y][x].is_mine) return "clicked_on_mine";
-    if (this.#board[y][x].is_open) return "already_open";
-
-    // if there's a neighbor mine, only open the current cell
-    if (this.neighbors_of(x, y).some((cell) => cell.is_mine)) return "open";
-
-    // otherwise we're wide open!
-    return "wide_open";
-  }
-
   click(x: number, y: number) {
-    switch (this.#try_click_at(x, y)) {
-      case "already_open":
-        return;
-      case "clicked_on_mine":
-        this.#board[y][x].is_open = true;
-        return;
-      case "open":
-        this.#board[y][x].is_open = true;
-        return;
-      case "out_of_range":
-        throw "out of range!";
-      case "wide_open":
-        this.#board[y][x].is_open = true;
+    if (x < 0 || x >= this.WIDTH || y < 0 || y >= this.HEIGHT)
+      throw "out_of_range";
 
-        // time to expand in each direction
-        for (let cell of this.neighbors_of(x, y)) this.click(cell.x, cell.y);
+    if (this.#board[y][x].is_mine) {
+      // open, and now the game is over
+      this.#board[y][x].is_open = true;
+    } else if (this.#board[y][x].is_open) {
+      // do nothing
+    } else if (this.number_of_mines_near(x, y) > 0) {
+      // only open current cell
+      this.#board[y][x].is_open = true;
+    } else {
+      // open current cell, and "click" each neighbor
+      this.#board[y][x].is_open = true;
+      for (let cell of this.neighbors_of(x, y)) this.click(cell.x, cell.y);
     }
   }
 
