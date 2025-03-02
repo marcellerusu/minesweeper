b5c18db20ba32416f996e43e8148601c8cc0bba1
MarcelRusu <marcelrusu@protonmail.com>
2025-03-01 22:07:32 -0500

    remove useState

--- src/src/Game.tsx
+++ src/src/Game.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useState } from "react";
+import React, { useEffect, useReducer } from "react";
 import type * as Types from "./types";
 import type { Point } from "./types";
 import Cell from "./Cell/Cell";
