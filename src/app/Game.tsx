55a9a23a6083df8b84f7153c6328e8b0fd2bd5ba
MarcelRusu <marcelrusu@protonmail.com>
2025-03-03 15:33:51 -0500

    game lost / won logic + msg

--- src/app/Game.tsx
+++ src/app/Game.tsx
@@ -1,10 +1,23 @@
+import "./Game.css";
 import React, { useEffect } from "react";
 import Board from "./Board/Board";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { space } from "./state/game";
+import { RootState } from "./store";
 
 function Game() {
   let dispatch = useDispatch();
+  let isGameLost = useSelector(({ game: { board } }: RootState) =>
+    board.some((row) => row.some((cell) => cell.isMine && cell.isOpen))
+  );
+  let isGameWon = useSelector(({ game: { board } }: RootState) =>
+    board.every((row) =>
+      row.every(
+        (cell) =>
+          ((cell.isMine || cell.isFlagged) && !cell.isOpen) || cell.isOpen
+      )
+    )
+  );
 
   useEffect(() => {
     let mouse = { x: 0, y: 0 };
@@ -26,7 +39,13 @@ function Game() {
     };
   }, []);
 
-  return <Board />;
+  return (
+    <div className="game">
+      <Board />
+      {isGameLost && <span className="game-over-msg">You lost</span>}
+      {isGameWon && <span className="game-won-msg">You won</span>}
+    </div>
+  );
 }
 
 export default Game;
