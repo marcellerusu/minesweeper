f2b9e2154e2ea7a2cafa35ddafeb3df49ae8482b
MarcelRusu <marcelrusu@protonmail.com>
2025-03-04 08:37:14 -0500

    replace handmade modal with dialog

--- src/app/Game/Header/Settings/Settings.tsx
+++ src/app/Game/Header/Settings/Settings.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { changeDifficulty } from "@/app/state/game";
 import "./Settings.css";
@@ -14,46 +14,47 @@ function Gear({ onClick }: { onClick: () => void }) {
   );
 }
 
-function SettingsForm({ onClose }: { onClose: () => void }) {
+function Settings() {
+  let dialog = useRef<HTMLDialogElement>(null);
   let dispatch = useDispatch();
   let { difficulty } = useSelector((state: RootState) => state.game.settings);
-  return (
-    <form className="settings-form">
-      <button
-        className="close"
-        onClick={(e) => {
-          e.preventDefault();
-          onClose();
-        }}
-      >
-        x
-      </button>
-      <h1>Settings</h1>
-      <select
-        name="difficulty"
-        defaultValue={difficulty}
-        onChange={(e) => {
-          let { difficulty } = e.currentTarget.closest("form")!.elements as any;
-          dispatch(changeDifficulty(difficulty.value));
-          onClose();
-        }}
-      >
-        <option value="beginner">Beginner (9 x 9 with 10 mines)</option>
-        <option value="intermediate">
-          Intermediate (16 x 16 with 40 mines)
-        </option>
-        <option value="expert">Expert (16 x 30 with 99 mines)</option>
-      </select>
-    </form>
-  );
-}
 
-function Settings() {
-  let [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <Gear onClick={() => setIsOpen(true)} />
-      {isOpen && <SettingsForm onClose={() => setIsOpen(false)} />}
+      <Gear onClick={() => dialog.current!.showModal()} />
+      <dialog
+        ref={dialog}
+        onMouseDown={(e) => {
+          // it appears that when the dialog is open
+          // it takes up the size of the entire screen
+          // and if you click, and it's not matching something directly inside the dialog
+          // like the form then it must be clicking on the backdrop
+          // and so we can close the dialog
+          let clickedOn = e.target as HTMLElement;
+          if (clickedOn.matches("dialog")) dialog.current!.close();
+        }}
+      >
+        <form className="settings-form" method="dialog">
+          <button className="close">x</button>
+          <h1>Settings</h1>
+          <select
+            name="difficulty"
+            defaultValue={difficulty}
+            onChange={(e) => {
+              let { difficulty } = e.currentTarget.closest("form")!
+                .elements as any;
+              dispatch(changeDifficulty(difficulty.value));
+              dialog.current!.close();
+            }}
+          >
+            <option value="beginner">Beginner (9 x 9 with 10 mines)</option>
+            <option value="intermediate">
+              Intermediate (16 x 16 with 40 mines)
+            </option>
+            <option value="expert">Expert (16 x 30 with 99 mines)</option>
+          </select>
+        </form>
+      </dialog>
     </>
   );
 }
