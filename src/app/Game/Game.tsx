69278b81802d4b90315fed8fafe4728338bf40ef
MarcelRusu <marcelrusu@protonmail.com>
2025-03-10 13:06:41 -0400

    comment the Game useEffect

--- src/app/Game/Game.tsx
+++ src/app/Game/Game.tsx
@@ -16,6 +16,22 @@ function Game() {
     let cellSize = localStorage.getItem("--cell-size") ?? "40";
     document.body.style.setProperty("--cell-size", `${cellSize}px`);
 
+    // all this is to be able to flag / expand a cell by pressing space
+    // we can't use traditional events on the given cell, since we aren't
+    // directly interacting with the cell
+    //
+    // instead, we track the mouse movement and `hover` the given cell
+    // by examining the cell html element at the mouse position
+    // and grabbing it's x/y coordinates sorted in data attributes
+    // this is significantly easier & faster than trying to figure this all out
+    // via react state.
+    //
+    // once a space is pressed, we flag / expand the currently hovered cell
+    //
+    // an optimization we could do is to not `hover` the cell on each mouse move
+    // but i like seeing the white border around the cell the mouse is under
+    // and as far as i can tell it has negligible performance cost
+
     function trackMouse(e: MouseEvent) {
       let cellHtml = document
         .elementsFromPoint(e.clientX, e.clientY)
