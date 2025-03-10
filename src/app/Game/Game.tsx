3828dd155acd5ef99b34fbb3a621263ac8f6903b
MarcelRusu <marcelrusu@protonmail.com>
2025-03-10 14:12:20 -0400

    clean up <Game>

--- src/app/Game/Game.tsx
+++ src/app/Game/Game.tsx
@@ -4,28 +4,31 @@ import { hover, space } from "@/app/state/game";
 import Board from "./Board/Board";
 import Header from "./Header/Header";
 
-function Game() {
+/**
+ * This is to be able to flag / expand a cell by pressing space while
+ * hovering a cell with the mouse.
+ *
+ * we can't use traditional events on the given cell, since we aren't
+ * directly interacting with the cell
+ *
+ * instead, we track the mouse movement and `hover` the given cell
+ * by examining the cell html element at the mouse position
+ * and grabbing it's x/y coordinates sorted in data attributes
+ * this is significantly easier & faster than trying to figure this all out
+ * via react state.
+ *
+ * once a space is pressed, we flag / expand the currently hovered cell
+ *
+ * an optimization we could do is to not `hover` the cell on each mouse move
+ * but i like seeing the white border around the cell the mouse is under
+ * and as far as i can tell it has negligible performance cost
+ */
+function useSpaceControls() {
   let dispatch = useDispatch();
 
   useEffect(() => {
     let cellSize = localStorage.getItem("--cell-size") ?? "40";
     document.body.style.setProperty("--cell-size", `${cellSize}px`);
-
-    // all this is to be able to flag / expand a cell by pressing space
-    // we can't use traditional events on the given cell, since we aren't
-    // directly interacting with the cell
-    //
-    // instead, we track the mouse movement and `hover` the given cell
-    // by examining the cell html element at the mouse position
-    // and grabbing it's x/y coordinates sorted in data attributes
-    // this is significantly easier & faster than trying to figure this all out
-    // via react state.
-    //
-    // once a space is pressed, we flag / expand the currently hovered cell
-    //
-    // an optimization we could do is to not `hover` the cell on each mouse move
-    // but i like seeing the white border around the cell the mouse is under
-    // and as far as i can tell it has negligible performance cost
 
     function trackMouse(e: MouseEvent) {
       let cellHtml = document
@@ -57,7 +60,10 @@ function Game() {
       window.removeEventListener("keydown", onKeydown);
     };
   }, []);
+}
 
+function Game() {
+  useSpaceControls();
   return (
     <div className="game">
       <Header />
